fb1644506635b14b55297d55b1a2fd6a47a1bff8
christie46 <ezechukwuobianuju01@gmail.com>
2022-10-12 14:31:53 +0000

    first commit

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,31 +1,33 @@
-
+#include <stdio.h>
+#include <stdlib.h>
 #include "3-calc.h"
-#include <string.h>
 
 /**
- * get_op_func - selects the correct operation toperform
- * @s: operation to perform
+ * main - performs simple operations
+ * @argc: number of arguments passed
+ * @argv: array of pointers to arguments
  *
- * Return: pointer to the correct function
+ * Return: always 0
  */
-int (*get_op_func(char *s))(int, int)
+int main(int argc, char *argv[])
 {
-	op_t ops[] = {
-		{"+", op_add},
-		{"-", op_sub},
-		{"*", op_mul},
-		{"/", op_div},
-		{"%", op_mod},
-		{NULL, NULL}
-	};
-	int i;
+	int a, b, c;
+	int (*f)(int, int);
 
-	i = 0;
-	while (ops[i].op != NULL)
+	if (argc != 4)
 	{
-		if (strcmp(s, ops[i].op) == 0)
-			break;
-		i++;
+		printf("Error\n");
+		exit(98);
 	}
-	return (ops[i].f);
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	f = get_op_func(argv[2]);
+	if (f == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	c = f(a, b);
+	printf("%d\n", c);
+	return (0);
 }
